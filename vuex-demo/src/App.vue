e1cfe3a33f89a35f35850db0022258332f43a14c
lifeblood123 <zhangyuner112@163.com>
2019-06-14 22:40:25 +0800

    state getters

---
+++ vuex-demo/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProdectListOne from './components/product-list-one.vue'
+import ProdectListTwo from './components/product-list-two.vue'
+
+export default {
+  name: 'app',
+  components:{
+    'product-list-one' : ProdectListOne,
+    'product-list-two' : ProdectListTwo
+  },
+  data () {
+    return {
+      // products:[
+      //   {name:"马云",price:200},
+      //   {name:"马化腾",price:140},
+
+      //   {name:"马冬梅",price:20},
+
+      //   {name:"马容",price:10},
+
+      // ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ vuex-demo/src/components/product-list-two.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="product-list-two">
+    <h2>two</h2>
+    <ul>
+      <li v-for="product in products">
+        <span class="name">{{product.name}}</span>
+        <span class="price">${{product.price}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+    saleProduct() {
+      var saleProduct = this.$store.state.products.map(product => {
+        return {
+          name: "**" + product.name + "**",
+          price: product.price / 2
+        };
+      });
+      return saleProduct;
+    }
+  }
+};
+</script>
+<style>
+#product-list-two {
+  background: rgb(37, 191, 230);
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
+}
+#product-list-two ul {
+  padding: 0;
+}
+#product-list-two li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
+}
+.price {
+  font-weight: bold;
+  color: blueviolet;
+}
+</style>
